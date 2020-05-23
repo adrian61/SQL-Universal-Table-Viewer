d4c7b8988aff1aeb9729ebb12f1e50a67ba1ee9b
adrian61 <ad.kowalskI@gmail.com>
2020-05-23 15:04:13 +0200

    Configured Spring Security Reformated structure of project

--- src/main/java/com/example/demo/web/controller/ApplicationController.java
+++ src/main/java/com/example/demo/web/controller/ApplicationController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.web.controller;
 
 import com.example.demo.core.jpa.dao.CourseDAO;
 import com.example.demo.core.jpa.domain.Course;
---
+++ src/main/java/com/example/demo/web/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.web.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		User.UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+				.withUser(users.username("test_admin").password("test").roles("ADMIN"))
+				.withUser(users.username("test_user").password("test").roles("USER"));
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").loginProcessingUrl("/auth_user").permitAll();
+	}
+}
