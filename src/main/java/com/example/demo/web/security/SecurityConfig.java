79ead132e0be4f38f23abb04ba8479a17a6ff2b8
adrian61 <ad.kowalskI@gmail.com>
2020-05-24 20:05:12 +0200

    Added JDBC auth

--- src/main/java/com/example/demo/web/security/SecurityConfig.java
+++ src/main/java/com/example/demo/web/security/SecurityConfig.java
@@ -1,24 +1,33 @@
 package com.example.demo.web.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		User.UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-				.withUser(users.username("test_admin").password("test").roles("ADMIN"))
-				.withUser(users.username("test_user").password("test").roles("USER"));
-
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username as principal, password as credentials, enabled from \"user\" where username = ?")
+				.authoritiesByUsernameQuery("select username as principal, role as role from \"user\" where username = ?")
+				.rolePrefix("ROLE_");
 	}
 
 	@Override
@@ -36,6 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 				.logoutSuccessUrl("/").deleteCookies("JSESSIONID")
 				.invalidateHttpSession(true);
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
